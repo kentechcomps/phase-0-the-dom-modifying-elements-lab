03388818c3bf4ac7baae37b9664236f831a440fe
user <mutukukennedy34@gmail.com>
2023-06-21 14:02:55 +0300

    modyfyingdom

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+
+const mainb =document.querySelector("#main")
+mainb.remove;
+
+const newHeader = document.createElement('h1');
+newHeader.nodeName ='victory'
+
+newHeader.id = 'victory'
+newHeader.append(newHeader)
+
+const newHeaders = document.querySelector('h1#victory')
+newHeaders.innerHTML = '<h1 id= "victory" > YOUR-NAME is the champion</h1>'
+
